ff1e5e5bdf79845a90dd6aa51135927ce37b784c
Echo <echorw927@gmail.com>
2026-01-12 20:32:18 -0800

    comit

--- sources-or-attributions-needed.md
+++ sources-or-attributions-needed.md
@@ -1,1 +1,4 @@
-bu
+https://crates.io/crates/tauri-plugin-screenshots
+https://github.com/ayangweb/tauri-plugin-screenshots/
+https://bun.com/docs/bundler 
+https://stackoverflow.com/questions/36911241/how-to-extract-data-out-of-a-promise 
--- src/out/main.js
+++ src/out/main.js
@@ -1,13 +1,10 @@
-const { invoke } = window.__TAURI__.core;
-
-let greetInputEl;
-let greetMsgEl;
-
+// src/main.js
+var { invoke } = window.__TAURI__.core;
+var greetInputEl;
+var greetMsgEl;
 async function greet() {
-
   greetMsgEl.src = await invoke("greet");
 }
-
 window.addEventListener("DOMContentLoaded", () => {
   greetInputEl = document.querySelector("#greet-input");
   greetMsgEl = document.querySelector("#ss-image");
