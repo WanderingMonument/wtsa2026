22afc45e806d7f75ddaf499e18902c3edfc236e6
Echo <echorw927@gmail.com>
2026-01-13 18:15:05 -0800

    sd

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using IgniteView.Core;
+using IgniteView.Desktop;
+
+public class Program
+{
+    [STAThread]
+    static void Main(string[] args)
+    {
+        DesktopPlatformManager.Activate();
+        var app = new ViteAppManager();
+
+        var mainWindow =
+            WebWindow.Create()
+            .WithTitle("Hello, world")
+            .Show();
+
+        app.Run();
+    }
+}
--- sources-or-attributions-needed.md
+++
@@ -1,4 +0,0 @@
-https://crates.io/crates/tauri-plugin-screenshots
-https://github.com/ayangweb/tauri-plugin-screenshots/
-https://bun.com/docs/bundler 
-https://stackoverflow.com/questions/36911241/how-to-extract-data-out-of-a-promise 
