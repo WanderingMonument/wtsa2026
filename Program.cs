b50b83fc48da775f1e44376e198573cd5d2fb94b
Echo <echorw927@gmail.com>
2026-01-15 17:40:02 -0800

    f

--- Program.cs
+++ Program.cs
@@ -1,6 +1,10 @@
 ﻿using IgniteView.Core;
 using IgniteView.Desktop;
+using System;
 using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.CompilerServices;
+
 
 public class Program
 {
@@ -20,10 +24,15 @@ public class Program
 }
 
 public class Commands
-{
+{ 
+
     [Command("screenshot")]
-    public static string Screenshot()
+    public static string Screenshot(int x, int y, int width, int height)
     {
+        Bitmap bmp = new Bitmap(width, height);
+        Graphics graphics = Graphics.FromImage(bmp);
+        graphics.CopyFromScreen(x, y, 0, 0, new Size(width, height));
+        bmp.Save("screenshot", ImageFormat.Jpeg);
         return ("this is a value returned from C#!");
     }
 }
