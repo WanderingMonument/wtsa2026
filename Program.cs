ddc7a175262cebf25a4f57125e49c71128c11582
Echo <echorw927@gmail.com>
2026-01-16 20:56:51 -0800

    Switch to using WPF instead of WinForms
    not included in a commit it just happend on my pc

--- Program.cs
+++ Program.cs
@@ -3,8 +3,8 @@ using IgniteView.Desktop;
 using System;
 using System.Drawing;
 using System.Drawing.Imaging;
-using System.Runtime.CompilerServices;
-
+using System.Windows;
+using wtsa2026;
 
 public class Program
 {
@@ -14,10 +14,14 @@ public class Program
         DesktopPlatformManager.Activate();
         var app = new ViteAppManager();
 
+    
+
         var mainWindow =
             WebWindow.Create()
-            .WithTitle("Hello, world")
+            .WithTitle("Visual Assistant")
             .Show();
+        
+            
 
         app.Run();
     }
@@ -29,10 +33,15 @@ public class Commands
     [Command("screenshot")]
     public static string Screenshot(int x, int y, int width, int height)
     {
+        
         Bitmap bmp = new Bitmap(width, height);
         Graphics graphics = Graphics.FromImage(bmp);
-        graphics.CopyFromScreen(x, y, 0, 0, new Size(width, height));
-        bmp.Save("screenshot", ImageFormat.Jpeg);
+        graphics.CopyFromScreen(x, y, 0, 0, new System.Drawing.Size(width, height));
+        bmp.Save("screenshot.jpeg", ImageFormat.Jpeg);
+        
+        Window1 window = new Window1();
+        window.Show();
+
         return ("this is a value returned from C#!");
     }
 }
--- src-vite/src/main.js
+++ src-vite/src/main.js
@@ -29,7 +29,7 @@ setupCounter(document.querySelector('#counter'))
 
 async function greet() {
 
-    const username = await igniteView.commandBridge.screenshot();
+    const username = await igniteView.commandBridge.screenshot(0,0,100,100);
     console.log(username);
 }
 
